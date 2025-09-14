37baf7fe4152b6b250d0661fe5a2722d4a20ceb5
KabeloMak <kabelo.makhetha17@gmail.com>
2025-09-14 21:15:13 +0200

    Commit Python files

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+current_age = int(input("How old are you?"))
+years_to_2050 = 27
+age = current_age + years_to_2050
+
+print(f"In 2050, you will be {age} years old")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2
+seconds_in_hours = 3600
+
+seconds = seconds_in_hours*hours
+
+print(hours, "hour(s) is", seconds)
